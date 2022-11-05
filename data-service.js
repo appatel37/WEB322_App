ef08b7e1614543a1652ddc87d73e37c7099d5936
Aryan Pareshbhai Patel <aryanpatel2602@gmail.com>
2022-11-04 23:18:35 -0400

    Assignment 4

--- data-service.js
+++ data-service.js
@@ -1,41 +1,107 @@
-var students, programs;
 var fs = require("fs");
+var students = [];
+var programs = [];
 
-module.exports.initialize = function () {
+module.exports.initialize = () => {
   return new Promise((resolve, reject) => {
     fs.readFile("./data/students.JSON", (err, data) => {
-      if (err) reject("File not read");
+      if (err) reject("Unable to read the file");
       students = JSON.parse(data);
-      resolve();
     });
     fs.readFile("./data/programs.JSON", (err, data) => {
-      if (err) reject("File not read");
-      students = JSON.parse(data);
-      resolve();
+      if (err) reject("Unable to read the file");
+      programs = JSON.parse(data);
     });
+    resolve();
   });
 };
 
-module.exports.getAllStudents = function () {
+module.exports.getAllStudents = () => {
   return new Promise((resolve, reject) => {
     if (students.length > 0) resolve(students);
-    else reject("No results returned");
+    else reject("No results");
   });
 };
 
-module.exports.getInternationalStudents = function () {
-  return new Promise((resolve, reject) => {
-    const internationalStu = students.filter((stud) => {
-      return stud.isInternationalStudent === true;
-    });
-    if (internationalStu.length > 0) resolve(internationalStu);
-    else reject("No results returned");
-  });
-};
-
-module.exports.getPrograms = function () {
+module.exports.getPrograms = () => {
   return new Promise((resolve, reject) => {
     if (programs.length > 0) resolve(programs);
-    else reject("No results returned");
+    else reject("No results");
+  });
+};
+
+module.exports.addStudent = (studentData) => {
+  return new Promise((resolve, reject) => {
+    if (typeof studentData.isInternationalStudent === undefined)
+      studentData.isInternationalStudent = false;
+    else studentData.isInternationalStudent = true;
+    var id = [], newID;
+    students.forEach((id) => {
+      id.push(parseInt(id.studentID));
+    });
+    newID = Math.max(id) + 1;
+    newID = toString(newID);
+    studentData.studentID = newID;
+    students.push(studentData);
+  });
+};
+
+module.exports.getStudentsByStatus = (status) => {
+  return new Promise((resolve, reject) => {
+    var statStu = students.filter((studentData) => {
+      return studentData.status === status;
+    });
+    if (statStu.length > 0) resolve(statStu);
+    else reject("No Results Returned");
+  });
+};
+
+module.exports.getStudentsByProgramCode = (programCode) => {
+  return new Promise((resolve, reject) => {
+    var studentData = students.filter((studentData) => {
+      return studentData.program === programCode;
+    });
+    if (studentData.length > 0) resolve(studentData);
+    else reject("No results");
+  });
+};
+
+module.exports.getStudentsByExpectedCredential = (credential) => {
+  return new Promise((resolve, reject) => {
+    var studentData = students.filter((studentData) => {
+      return studentData.expectedCredential === credential;
+    });
+    if (studentData.length > 0) resolve(studentData);
+    else reject("No Results Returned");
+  });
+};
+
+module.exports.getStudentById = (studentID) => {
+  return new Promise((resolve, reject) => {
+    var studentID = students.filter((studentData) => {
+      return studentData.studentID === studentID;
+    });
+    if (studentID.length > 0) resolve(studentID[0]);
+    else reject("No Results Returned");
+  });
+};
+
+module.exports.updateStudent = (studentData) => {
+  return new Promise((resolve, reject) => {
+    students.forEach((studentData) => {
+      if (studentData.studentID === studentData.studentID) {
+        studentData.studentID = studentData.studentID;
+        studentData.firstName = studentData.firstName;
+        studentData.lastName = studentData.lastName;
+        studentData.program = studentData.program;
+        studentData.expectedCredential = studentData.expectedCredential;
+        studentData.status = studentData.status;
+        studentData.isInternationalStudent = studentData.isInternationalStudent;
+        resolve();
+      }
+      else{
+        reject("Student not found");
+      }
+    });
   });
 };
--- server.js
+++ server.js
@@ -5,73 +5,196 @@
 * 
 *  Name: Aryan Pareshbhai Patel Student ID: 133889212 Date: 30/09/2022
 *
-*  Online (Cyclic) Link: 
+*  Online (Cyclic) Link: https://cheerful-school-uniform-lion.cyclic.app/
 *
 ********************************************************************************/
 
-var exp = require("express");
-var app = exp();
-var jsonData = require("./data-service.js");
+var express = require("express");
+
+var app = express();
+
 var path = require("path");
-const { json } = require("body-parser");
+
+var data = require("./data-service");
+
+var multer = require("multer");
+
+var fs = require("fs");
+
+var exphbs = require("express-handlebars");
+
+var { equal } = require("assert");
+
 var HTTP_PORT = process.env.PORT || 8080;
 
-app.use(exp.static("public/css"));
+app.use(express.static("public"));
 
-function onStart() {
-  console.log("Server listening on port " + HTTP_PORT);
+app.use(express.urlencoded({ extended: true }));
+
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    extname: ".hbs",
+    helpers: {
+      navLink: (url, options) => {
+        return (
+          "<li" +
+          (url == app.locals.activeRoute ? ' class="active" ' : "") +
+          '><a href="' +
+          url +
+          '">' +
+          options.fn(this) +
+          "</a></li>"
+        );
+        equal: f;
+      },
+      equal: (lvalue, rvalue, options) => {
+        if (arguments.length < 3)
+          throw new error("Handlebars Helper equal needs 2 parameters");
+        if (lvalue != rvalue) {
+          return options.inverse(this);
+        } else {
+          return options.fn(this);
+        }
+      },
+    },
+  })
+);
+
+app.set("view engine", ".hbs");
+
+var onHTTPStart=() => {
+  console.log("Express http server listening on port " + HTTP_PORT);
 }
 
+var storage = multer.diskStorage({
+  destination: "./public/images/uploaded",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+var upload = multer({ storage: storage });
+
+app.use(function (req, res, next) {
+  let route = req.baseUrl + req.path;
+  app.locals.activeRoute = route == "/" ? "/" : route.replace(/\/$/, "");
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"));
+  res.render("home");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/about.html"));
+  res.render("about");
 });
 
 app.get("/students", (req, res) => {
-  jsonData
-    .getAllStudents()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json({ Message: "Error" });
-    });
+  if (req.query.status) {
+    data
+      .getStudentsByStatus(req.query.status)
+      .then((data) => {
+        res.render("students", { students: data });
+      })
+      .catch((error) => {
+        res.json({ Message: "error" });
+      });
+  } else if (req.query.program) {
+    data
+      .getStudentsByProgramCode(req.query.program)
+      .then((data) => {
+        res.render("students", { students: data });
+      })
+      .catch((error) => {
+        res.json({ Message: "error" });
+      });
+  } else if (req.query.credential) {
+    data
+      .getStudentsByExpectedCredential(req.query.credential)
+      .then((data) => {
+        res.render("students", { students: data });
+      })
+      .catch((error) => {
+        res.json({ Message: "error" });
+      });
+  } else {
+    data
+      .getAllStudents()
+      .then((data) => {
+        res.render("students", { students: data });
+      })
+      .catch((error) => {
+        res.render("students", { message: "no results" });
+      });
+  }
 });
 
-app.get("/intlstudents", (req, res) => {
-  jsonData
-    .getInternationalStudents()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json({ Message: "Error" });
+app.post("/student/update", (req, res) => {
+  data
+    .updateStudent(req.body)
+    .then(res.redirect("/students"))
+    .catch((error) => {
+      console.log("error", error);
     });
 });
 
 app.get("/programs", (req, res) => {
-  jsonData
-    .getAllStudents()
+  data
+    .getPrograms()
     .then((data) => {
-      res.json(data);
+      res.render("programs", { programs: data });
     })
-    .catch((err) => {
-      res.json({ Message: "Error" });
+    .catch((error) => {
+      res.json({ Message: "error" });
+    });
+});
+
+app.get("/students/add", (req, res) => {
+  res.render("addStudent");
+});
+
+app.get("/images/add", (req, res) => {
+  res.render("addImage");
+});
+
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+  res.redirect("/images");
+});
+
+app.get("/images", (req, res) => {
+  fs.readdir("./public/images/uploaded", (error, data) => {
+    if (error) console.log("error in reading the directory.");
+    else {
+      res.render("images", { images: data });
+    }
+  });
+});
+
+app.get("/student/:studentId", (req, res) => {
+  data
+    .getStudentById(req.params.studentId)
+    .then((data) => {
+      res.render("student", { student: data });
+    })
+    .catch((error) => {
+      res.render("student", { message: "no results" });
     });
 });
 
 app.use((req, res) => {
-  res.status(404).send("Page not found.");
+  res
+    .status(404)
+    .send(
+      "Error 404. Page not found."
+    );
 });
 
-jsonData
+data
   .initialize()
   .then(() => {
-    app.listen(HTTP_PORT, onStart);
+    app.listen(HTTP_PORT, onHTTPStart);
   })
-  .catch((err) => {
-    console.log("Error");
+  .catch((error) => {
+    console.log("error");
   });
